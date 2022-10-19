802942b214891d879a78cc4ea5ff71da8ef7f3e9
Sonam Pal <spal@ukcloud.com>
2022-10-19 14:17:20 +0100

    deploying on heroku

--- route.spec.js
+++ route.spec.js
@@ -5,7 +5,7 @@ describe('Get route', () => {
   it('page should return hello world travis', async () => {
     const res = await request(app).get('/')
     expect(res.statusCode).toEqual(200)
-    expect(res.body).toEqual('Hello world from Travis')
+    expect(res.body).toEqual('Hello world from Travis. CI.CD Challenge')
   })
 })
 
