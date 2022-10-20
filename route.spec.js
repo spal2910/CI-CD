7162e812757fa13675c1565d893f50c654774791
Sonam Pal <spal@ukcloud.com>
2022-10-20 09:06:48 +0100

    change to index.js

--- route.spec.js
+++ route.spec.js
@@ -2,7 +2,7 @@ const request = require('supertest') //importing the supertest library
 const app = require('./index.js') //importing the 'index.js' file
 
 describe('Get route', () => {
-  it('page should return hello world travis', async () => {
+  it('page should return hello world travis. CI.CD Challenge', async () => {
     const res = await request(app).get('/')
     expect(res.statusCode).toEqual(200)
     expect(res.body).toEqual('Hello world from Travis. CI.CD Challenge')
